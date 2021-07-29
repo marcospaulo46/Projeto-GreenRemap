21bb8ae79c13b1174777f90b6ce57c64fdfd23ad
Daiane Feliciano <77757301+DaianeFeliciano@users.noreply.github.com>
2021-07-29 10:59:58 -0300

    camada anotações

--- greenremap/src/main/java/com/generationgreenremap/model/Categoria.java
+++ greenremap/src/main/java/com/generationgreenremap/model/Categoria.java
@@ -1,14 +1,32 @@
 package com.generationgreenremap.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+
+@Entity
+@Table(name = "categoria")
 public class Categoria {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotNull(message = "O atributo nome da categoria é Obrigatório!")
+	@Size(min = 5, max = 100)
 	private String nome_categoria;
 	
+	@Size(min = 5, max = 100, message = "O atributo caracteristica deve conter no mínimo 5 e no  máximo 100 caracteres")
 	private String palavra_chave;
 	
+	@Size(min = 5, max = 5000, message = "O atributo caracteristica deve conter no mínimo 5 e no  máximo 500 caracteres")
 	private String caracteristica;
+	
+	
 
 }
