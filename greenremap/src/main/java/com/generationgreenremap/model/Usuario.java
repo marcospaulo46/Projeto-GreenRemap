30a2a7c165e88e31fd35700b13d2fb9ce9f2fc41
basanntana <barbarasantanabr@gmail.com>
2021-07-29 15:05:45 -0300

    Get and Set

--- greenremap/src/main/java/com/generationgreenremap/model/Usuario.java
+++ greenremap/src/main/java/com/generationgreenremap/model/Usuario.java
@@ -42,4 +42,53 @@ public class Usuario {
 	@JsonIgnoreProperties("Usuario")
 	private List<Produto> produto;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	
 }
